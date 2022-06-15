eca6764470f576b5cbf9a6d908f89acd20816258
pablolucas890 <pablolucas890@gmail.com>
2022-06-15 08:21:44 -0300

    update

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ function cadastrar() {
     }
 
     if (document.getElementById("descricao").value == "") {
-        document.getElementById("descricao-span").innerHTML = "Descroção Inválido"
+        document.getElementById("descricao-span").innerHTML = "Descrição Inválido"
     } else {
         document.getElementById("descricao-span").innerHTML = " "
     }
@@ -40,9 +40,45 @@ function limpar() {
     document.getElementById('descricao').value = '';
     document.getElementById('marca').value = 'Volkswagen';
     document.getElementById('cor').value = 'Cinza';
+    document.getElementById('modelo-span').innerHTML = '';
+    document.getElementById('ano-span').innerHTML = '';
+    document.getElementById('preco-span').innerHTML = '';
+    document.getElementById('descricao-span').innerHTML = '';
 }
 function irEditar(){
     window.location = 'editar.html'
+}
+function excluir(div) {
+    if(div== 'Volkswagen'){
+        element = document.getElementById("vwid");
+        element2 = document.getElementById("vwid2");
+        element.remove()
+        element2.remove()
+    }
+    if(div== 'BMW'){
+        element = document.getElementById("bmwid");
+        element2 = document.getElementById("bmwid2");
+        element.remove()
+        element2.remove()
+    }
+    if(div== 'Honda'){
+        element = document.getElementById("Hondaid");
+        element2 = document.getElementById("Hondaid2");
+        element.remove()
+        element2.remove()
+    }
+    if(div== 'Volkswagen2'){
+        element = document.getElementById("Volkswagen2id");
+        element2 = document.getElementById("Volkswagen2id2");
+        element.remove()
+        element2.remove()
+    }
+    if(div== 'Fiat'){
+        element = document.getElementById("Fiatid");
+        element2 = document.getElementById("Fiatid2");
+        element.remove()
+        element2.remove()
+    }
 }
 (function () {
     VMasker(document.getElementById("preco")).maskMoney({ unit: 'R$' });
